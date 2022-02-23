d024154d8fa57a24971a3728d67be9595db0c664
Quang Trung <quangtrung@MacBook-Air-cua-Quang.local>
2022-02-23 14:21:20 +0700

    update

--- app/Http/Livewire/TagIndex.php
+++ app/Http/Livewire/TagIndex.php
@@ -8,29 +8,57 @@ use Illuminate\Support\Str;
 
 class TagIndex extends Component
 {
-    public $showTagModal=false;
+    public $showTagModal = false;
     public $tagName;
-    public $tags=[];
+    public $tags = [];
+    public $tagId;
 
     public function showCreateModal()
     {
-        $this->showTagModal=true;
+        $this->showTagModal = true;
+    }
+
+    public function mount()
+    {
+        $this->tags = Tag::all();
     }
     public function createTag()
     {
         Tag::create([
-            'tag_name'=>$this->tagName,
-            'slug'   =>Str::slug($this->tagName)
+            'tag_name' => $this->tagName,
+            'slug'   => Str::slug($this->tagName)
         ]);
         $this->reset();
     }
+    public function showEditModal($tagId)
+    {
+        $this->reset(['tagName']);
+        $this->tagId = $tagId;
+        $tag = Tag::find($tagId);
+        $this->tagName = $tag->tag_name;
+        $this->showTagModal = true;
+    }
+    public function updateTag()
+    {
+        $tag = Tag::findOrFail($this->tagId);
+        $tag->update([
+            'tag_name' => $this->tagName,
+            'slug' => Str::slug($this->tagName)
+        ]);
+        $this->reset();
+        $this->tags = Tag::all();
+        $this->showTagModal = false;
+    }
+    public function deleteTag($tagId)
+    {
+        $tag = Tag::findOrFail($tagId);
+        $tag->delete();
+        $this->reset();
+        $this->tags = Tag::all();
+    }
     public function closetagModal()
     {
-        $this->showTagModal=false;
-    }
-    public function mount()
-    {
-        $this->tags=Tag::all();
+        $this->showTagModal = false;
     }
 
     public function render()
--- resources/views/livewire/tag-index.blade.php
+++ resources/views/livewire/tag-index.blade.php
@@ -1,78 +1,88 @@
-<div>
-<div>
-    <!-- component -->
-<table class="border-collapse w-full ">
-    <div class="w-full flex mb-4 p-2 justify-end"><x-jet-button wire:click="showCreateModal">Create Tags</x-jet-button></div>
-    
-    <thead>
-        <tr>
-            <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">Name</th>
-            <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">Slug</th>
-            <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">Manage</th>
+<section>
+  <!-- component -->
+  <table class="border-collapse w-full ">
+    <div class="w-full flex mb-4 p-2 justify-end">
+      <x-jet-button wire:click="showCreateModal">Create Tags</x-jet-button>
+    </div>
 
-        </tr>
+    <thead>
+      <tr>
+        <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">Name</th>
+        <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">Slug</th>
+        <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">Manage</th>
+
+      </tr>
     </thead>
-    <tbody >
+    <tbody>
       @forelse($tags as $tag)
       <tr class="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0">
-            <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
-                <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Company name</span>
-               {{$tag->tag_name}}
-            </td>
-            <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
-                <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Country</span>
-                {{$tag->slug}}
-            </td>
-          
-            
-            <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
-                Edit/Delete
-            </td>
-        </tr>
+        <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+          <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Company name</span>
+          {{$tag->tag_name}}
+        </td>
+        <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
+          <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Country</span>
+          {{$tag->slug}}
+        </td>
+
+
+        <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
+          <x-m-button wire:click="showEditModal({{$tag->id}})" class="bg-green-500 hover:bg-green-700 text-white">Edit</x-m-button>
+          <x-m-button wire:click="deleteTag({{$tag->id}})" class="bg-red-500 hover:bg-red-700 text-white">Delete</x-m-button>
+        </td>
+      </tr>
 
       @empty
-      <tr class="text-gray-700"> 
+      <tr class="text-gray-700">
         <td class="px-4 py-3 boder ">
           Empty
         </td>
       </tr>
       @endforelse
-        
-       
-       
+
+
+
     </tbody>
-</table>
-<x-jet-dialog-modal wire:model="showTagModal">
-<x-slot name="title">Create Tag</x-slot>
-<x-slot name="content">
-<div class="mt-10 sm:mt-0">
-  
-    <div class="md:col-span-1">
-     
-    </div>
-    <div class="mt-5 md:mt-0 md:col-span-2">
-      <form >
-        <div class="shadow overflow-hidden sm:rounded-md">
-          <div class="px-4 py-5 bg-white sm:p-6">
-            <div class="grid grid-cols-6 gap-6">
-              <div class="col-span-6">
-                <label for="street-address" class="block text-sm font-medium text-gray-700">Tag Name</label>
-                <input wire:model="tagName" type="text" name="street-address" id="street-address" autocomplete="street-address" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-              </div>   
-          </div>
+  </table>
+  <x-jet-dialog-modal wire:model="showTagModal">
+    @if ($tagId)
+    <x-slot name="title">Update Tag</x-slot>
+    @else
+    <x-slot name="title">Create Tag</x-slot>
+    @endif
+
+    <x-slot name="content">
+      <div class="mt-10 sm:mt-0">
+
+        <div class="md:col-span-1">
+
         </div>
-      </form>
-    </div>
-  </div>
-</div>
+        <div class="mt-5 md:mt-0 md:col-span-2">
+          <form>
+            <div class="shadow overflow-hidden sm:rounded-md">
+              <div class="px-4 py-5 bg-white sm:p-6">
+                <div class="grid grid-cols-6 gap-6">
+                  <div class="col-span-6">
+                    <label for="street-address" class="block text-sm font-medium text-gray-700">Tag Name</label>
+                    <input wire:model="tagName" type="text" name="street-address" id="street-address" autocomplete="street-address" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                  </div>
+                </div>
+              </div>
+          </form>
+        </div>
+      </div>
+      </div>
 
-</x-slot>
-<x-slot name="footer">
-<x-jet-button wire:click="closetagModal" >Cancel</x-jet-button>
-<x-jet-button wire:click="createTag" >Create</x-jet-button>
+    </x-slot>
+    <x-slot name="footer">
+      <x-jet-button wire:click="closetagModal" class="bg-gray-600 hover:bg-gray-800 text-white">Cancel</x-jet-button>
+      @if($tagId)
+      <x-m-button wire:click="updateTag" class="">Update</x-m-button>
+      @else
+      <x-m-button wire:click="createTag" class="">Create</x-m-button>
 
-</x-slot>
-</x-jet-dialog-modal>
-</div>
+      @endif
 
-</div>
+    </x-slot>
+  </x-jet-dialog-modal>
+</section>
